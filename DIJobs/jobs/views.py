e3307ccf1a2655596cb02179f9bd19e9598ee003
Raayhan <rayhan.rakib@northsouth.edu>
2024-06-07 00:02:14 +0600

    Create API view and url paths

--- DIJobs/jobs/views.py
+++ DIJobs/jobs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Job
+from .models import Category
+from .serializers import JobSerializer,CategorySerializer
+
+
+class LatestJobsList(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, format=None):
+        jobs = Job.objects.all()[0:4]
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data)
+    
+
