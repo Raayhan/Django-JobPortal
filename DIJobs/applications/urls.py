087f884215ca5f9efa67e82b1662efd9d08cd40c
Raayhan <rayhan.rakib@northsouth.edu>
2024-06-10 12:42:07 +0600

    create API view for submitting job application

--- DIJobs/applications/urls.py
+++ DIJobs/applications/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path,include
 from applications import views
 
+from .views import SubmitApplication
+
 urlpatterns = [
     
+    path('jobs/apply/<int:job_id>/', SubmitApplication.as_view()),
 ]
--- DIJobs/applications/views.py
+++ DIJobs/applications/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Application, Job
+from .serializers import ApplicationSerializer
+
+class SubmitApplication(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, job_id):
+        try:
+            job = Job.objects.get(id=job_id)
+        except Job.DoesNotExist:
+            return Response({"error": "Job not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        data = request.data.copy()
+        data['candidate_id'] = request.user.id
+        data['job_id'] = job.id
+        
+        serializer = ApplicationSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
