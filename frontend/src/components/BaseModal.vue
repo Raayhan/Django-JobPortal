a8343227a46b49cb9958ffe0b1b5823eadb65ee9
Raayhan <rayhan.rakib@northsouth.edu>
2024-06-23 14:43:27 +0600

    Show Modal for Withdraw application

---
+++ frontend/src/components/BaseModal.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'BaseModal',
+    props: {
+        show: {
+            type: Boolean,
+        }
+    }
+};
+</script>
+
+<template>
+    <!-- Render inside our `<div id="modals"></div>` in index.html -->
+    <Teleport to="#modals">
+        <!-- Show / hide the modal -->
+        <div v-if="show" class="">
+            <!-- The backdrop -->
+            <div class="fixed inset-0 bg-gray-900 opacity-40 duration-300"></div>
+
+            <!-- Where the actual content goes -->
+            <div class="fixed inset-0 flex items-center justify-center">
+                <div class="bg-white text-gray-600 rounded">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+    </Teleport>
+</template>
--- frontend/src/views/Applications.vue
+++ frontend/src/views/Applications.vue
@@ -35,8 +35,9 @@
           <tr v-for="application in applications" v-bind:key="application.id" v-bind:application="application"
             class="bg-white border-b">
             <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap">
-              <router-link class="text-indigo-800 hover:underline" :to="application.job.get_absolute_url"> {{ application.job.title }}</router-link>
-              </th>
+              <router-link class="text-indigo-800 hover:underline" :to="application.job.get_absolute_url"> {{
+                application.job.title }}</router-link>
+            </th>
             <td class="">
               {{ application.job.company }}
             </td>
@@ -53,7 +54,8 @@
               {{ formatDate(application.job.deadline) }}
             </td>
             <td class="">
-              <button class="text-red-700 hover:font-bold">Withdraw</button>
+              <button type="button" @click="showModal(application)"
+                class="text-red-700 hover:font-bold">Withdraw</button>
             </td>
           </tr>
 
@@ -63,23 +65,61 @@
 
 
   </div>
+  <BaseModal :show="showModalFlag">
+    <div class="p-12">
+      <div class="text-xl text-center text-red-800 font-medium">Withdraw Confirmation !</div>
+
+      <div class="py-2 text-sm text-center">Are you sure you want to withdraw the application? <br>Please note, once it
+        is withdrawn you can not revert it.</div>
+      <div class="flex justify-center mt-4">
+        <form @submit.prevent="handleSubmit">
+
+          <input id="id" type="hidden" v-model="form.id" />
+          <button type="submit" :class="{ 'bg-red-500 animate-pulse': isSubmitting, 'bg-red-700': !isSubmitting }"
+            :disabled="isSubmitting" class="px-2 py-1 font-medium w-20 rounded-l mr-1 text-white hover:bg-red-800"
+            @click="showModal = false">
+            Confirm
+          </button>
+        </form>
+
+        <button type="button" class="bg-gray-700 px-2 py-1 font-medium w-20 rounded-r text-white hover:bg-gray-800"
+          @click="hideModal">
+          Close
+        </button>
+      </div>
+
+
+    </div>
+  </BaseModal>
 </template>
 <script>
 import axios from 'axios'
 import moment from 'moment';
+import { ref } from 'vue';
+import BaseModal from '@/components/BaseModal.vue'
 
 export default {
   name: 'Applications',
+  components: {
+    BaseModal
+  },
   data() {
     return {
       applications:[],
       statuses: {
-        '0': { class: 'text-xs text-violet-700 bg-violet-100 font-bold tracking-wider capitalized rounded py-1 px-2', text: 'Application Received' },
-        '1': { class: 'text-xs text-yellow-700 bg-yellow-100 font-bold capitalized tracking-wider rounded py-1 px-2', text: 'Under Consideration' },
-        '2': { class: 'text-xs text-green-700 bg-green-100 font-bold capitalized tracking-wider rounded py-1 px-2', text: 'Accepted' },
-        '3': { class: 'text-xs text-red-700 bg-red-100 font-bold capitalized tracking-wider rounded py-1 px-2', text: 'Rejected' },
+        '0': { class: 'text-xs text-violet-700 bg-violet-100 tracking-wider capitalized rounded py-1 px-2', text: 'Application Received' },
+        '1': { class: 'text-xs text-yellow-700 bg-yellow-100 capitalized tracking-wider rounded py-1 px-2', text: 'Under Consideration' },
+        '2': { class: 'text-xs text-green-700 bg-green-100 capitalized tracking-wider rounded py-1 px-2', text: 'Accepted' },
+        '3': { class: 'text-xs text-red-700 bg-red-100 capitalized tracking-wider rounded py-1 px-2', text: 'Rejected' },
 
       },
+      activeApplication: null,
+      form: {
+        id: '',
+        errors: []
+      },
+      isSubmitting: false,
+      showModalFlag: false,
     }
     
   },
@@ -90,6 +130,15 @@ export default {
 
   },
   methods: {
+
+    showModal(application) {
+      this.activeApplication = application;
+      this.form.id = application.id; // Set the form id to the application id
+      this.showModalFlag = true;
+    },
+    hideModal() {
+      this.showModalFlag = false;
+    },
 
     async getApplications() {
       this.$store.commit('setIsLoading', true)
@@ -111,9 +160,46 @@ export default {
        
     },
 
+    async handleSubmit() {
+      //console.table(this.form);
+      this.form.errors = []
+
+      if (!this.form.errors.length) {
+        this.showModalFlag = false;
+        this.isSubmitting = true;
+        const formData = new FormData();
+        formData.append('id', this.form.id);
+        console.log('Form Data:', this.form);
+        try {
+          //console.table(form)
+          const response = await axios.post("/api/v1/applications/withdraw", form);
+          this.$router.push('/applications')
+
+        } catch (error) {
+          if (error.response) {
+            for (const property in error.response.data) {
+
+              const errors = error.response.data[property];
+              if (Array.isArray(errors)) {
+                errors.forEach(err => this.form.errors.push(err));
+              } else {
+                this.form.errors.push(errors);
+              }
+            }
+          } else {
+            this.form.errors.push('Something went wrong. Please try again later.');
+            console.log(JSON.stringify(error));
+          }
+        } finally {
+          this.isSubmitting = false;
+        }
+      }
+    },
+
+
     formatDate(date) {
 
-      return moment(date).format('DD-MM-YYYY');
+      return moment(date).format('DD/MM/YYYY');
     },
     statusClass(status) {
       return this.statuses[status]?.class || '';
